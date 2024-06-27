6f5626703a8b8e6adc56444154c9a5ba1eee537a
NickWeller <nickweller2@gmail.com>
2024-06-26 20:37:30 -0500

    Exceptions

---
+++ functions/exercises/functions.js
@@ -0,0 +1,23 @@
+
+function makeLine(size) {
+    let line = '';
+    for (let i = 0; i < size; i++) {
+        line += '#'
+    }
+    return line;
+  }
+
+  function makeSquare(size) {
+    let square = '';
+    for (let j = 0; j < size; j++) {
+        square+= '\n' + makeLine(5) 
+    }
+    return square;
+  }
+
+function makeRectangle(width, height) {
+    let rectangle = ''
+    for (let k = 0; k < height;k++) {
+
+    }
+}
