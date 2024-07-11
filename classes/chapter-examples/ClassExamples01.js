f2cff7f0c6011cc114d8316cf70c32803a8c1da3
NickWeller <nickweller2@gmail.com>
2024-07-11 13:38:35 -0500

    class assignment

--- classes/chapter-examples/ClassExamples01.js
+++ classes/chapter-examples/ClassExamples01.js
@@ -1,19 +1,19 @@
 //Try adding new properties inside constructor.
 class Astronaut {
-   constructor(name, age, mass){
-     this.name = name;
-     this.age = age;
-     this.mass = mass;
-   }
+  constructor(name, age, mass) {
+    this.name = name;
+    this.age = age;
+    this.mass = mass;
+  }
 }
 
-let fox = new Astronaut('Fox', 7, 12);
+let fox = new Astronaut("Fox", 7, 12);
 
 console.log(fox);
 console.log(fox.age, fox.color);
 
 fox.age = 9;
-fox.color = 'red';
+fox.color = "red";
 
 console.log(fox);
 console.log(fox.age, fox.color);
--- classes/exercises/ClassExercises.js
+++ classes/exercises/ClassExercises.js
@@ -1,10 +1,108 @@
 // Define your Book class here:
-
+class Book {
+  constructor(
+    title,
+    author,
+    copyrightDate,
+    isbn,
+    numberOfPages,
+    timesCheckedOut,
+    discarded,
+  ) {
+    this.title = title;
+    this.author = author;
+    this.copyrightDate = copyrightDate;
+    this.isbn = isbn;
+    this.numberOfPages = numberOfPages;
+    this.timesCheckedOut = timesCheckedOut;
+    this.discarded = discarded;
+  }
+  checkout(uses = 1) {
+    this.timesCheckedOut += uses;
+    return this.timesCheckedOut;
+  }
+}
 
 // Define your Manual and Novel classes here:
+class Manual extends Book {
+  constructor(
+    title,
+    author,
+    copyrightDate,
+    isbn,
+    numberOfPages,
+    timesCheckedOut,
+    discarded,
+  ) {
+    super(
+      title,
+      author,
+      copyrightDate,
+      isbn,
+      numberOfPages,
+      timesCheckedOut,
+      discarded,
+    );
+  }
+  dispose(currentYear) {
+    if (currentYear - this.copyrightDate > 5) {
+      this.discarded = "Yes";
+    }
+  }
+}
 
-
+class Novel extends Book {
+  constructor(
+    title,
+    author,
+    copyrightDate,
+    isbn,
+    numberOfPages,
+    timesCheckedOut,
+    discarded,
+  ) {
+    super(
+      title,
+      author,
+      copyrightDate,
+      isbn,
+      numberOfPages,
+      timesCheckedOut,
+      discarded,
+    );
+  }
+  dispose() {
+    if (this.timesCheckedOut > 100) {
+      this.discarded = "Yes";
+    }
+  }
+}
 // Declare the objects for exercises 2 and 3 here:
+let prideAndPrejudice = new Novel(
+  "Pride and Prejudice",
+  "Jane Austen",
+  1813,
+  1111111111111,
+  432,
+  100,
+  "No",
+);
 
+let shuttleBuildingManual = new Manual(
+  "Top Secret Shuttle Building Manual",
+  "Redacted",
+  2013,
+  22222222222,
+  1147,
+  1,
+  "No",
+);
 
 // Code exercises 4 & 5 here:
+
+prideAndPrejudice.checkout(5);
+prideAndPrejudice.dispose();
+
+shuttleBuildingManual.dispose(2024);
+console.log(prideAndPrejudice);
+console.log(shuttleBuildingManual);
